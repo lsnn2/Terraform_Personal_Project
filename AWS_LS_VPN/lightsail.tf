b04176f82817c696d19cc9333ff9de830a35799e
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-06-08 14:55:07 -0400

    Update lightsail.tf

--- AWS_LS_VPN/lightsail.tf
+++ AWS_LS_VPN/lightsail.tf
@@ -1,1 +1,60 @@
+provider "aws" {
 
+  region = "eu-west-2"
+}
+
+variable "privid" {
+  type        = string
+}
+variable "pubid" {
+  type        = string
+}
+
+resource "aws_lightsail_instance" "outline-instance" {
+  name              = "outline-instance"
+  availability_zone = "eu-west-2a"
+  blueprint_id      = "ubuntu_18_04"
+  bundle_id         = "nano_2_0"
+  key_pair_name     = aws_lightsail_key_pair.outline-key.name
+  provisioner "file" {
+    source      = "DeployOutlineVPN.sh"
+    destination = "/tmp/DeployOutlineVPN.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/DeployOutlineVPN.sh"
+      , "sudo /tmp/DeployOutlineVPN.sh"
+    ]
+
+  }
+  connection {
+    user        = "ubuntu"
+    private_key = var.privid
+    host        = aws_lightsail_instance.outline-instance.public_ip_address
+  }
+}
+resource "aws_lightsail_key_pair" "outline-key" {
+  name       = "outline-key"
+  public_key = var.pubid
+}
+
+resource "aws_lightsail_instance_public_ports" "outline" {
+  instance_name = aws_lightsail_instance.outline-instance.name
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 0
+    to_port   = 65535
+  }
+  port_info {
+    protocol  = "udp"
+    from_port = 0
+    to_port   = 65535
+  }
+  port_info {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+    cidrs     = ["142.113.226.55/32"]
+  }
+}
