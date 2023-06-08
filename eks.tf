fa5685859c3bda7db8dedc0423a58d1b87f0a55d
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-06-08 14:30:49 -0400

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -67,17 +67,17 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t2.small"]
+      instance_types = ["t2.micro"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 2
+      desired_size = 1
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t2.small"]
+      instance_types = ["t2.micro"]
 
       min_size     = 1
       max_size     = 2
