5501580684f33e87f43decb52cc6bebfda122e58
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-06-09 19:10:40 -0400

    Update pgadmin4.sh

--- AWS-PGADMIN4/pgadmin4.sh
+++ AWS-PGADMIN4/pgadmin4.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install -y postgresql12-server
+sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+sudo systemctl enable postgresql-12
+sudo systemctl start postgresql-12
+sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
+sudo yum install -y pgadmin4-web
+sudo /usr/pgadmin4/bin/setup-web.sh -y
