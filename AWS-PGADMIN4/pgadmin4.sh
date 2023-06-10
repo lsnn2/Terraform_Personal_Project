96cc92205c7ffeae915e180c9651693a4c84e118
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-06-09 20:45:52 -0400

    Update pgadmin4.sh

--- AWS-PGADMIN4/pgadmin4.sh
+++ AWS-PGADMIN4/pgadmin4.sh
@@ -7,5 +7,26 @@ sudo systemctl enable postgresql-12
 sudo systemctl start postgresql-12
 sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
 sudo yum install -y pgadmin4-web
-echo -e "edwardli105@gmail.com\npassword\npassword" | sudo /usr/pgadmin4/bin/setup-web.sh --yes
+sudo yum install expect
+cat << 'EOF' > pgadmin-setup.sh
+#!/usr/bin/expect -f
+spawn sudo /usr/pgadmin4/bin/setup-web.sh --yes
+expect "Enter the email address and password to use for the initial pgAdmin user account:"
+send "email@example.com\r"
+expect "Email address:"
+send "password\r"
+expect "Password:"
+send "password\r"
+interact
+EOF
+chmod +x pgadmin-setup.sh
+./pgadmin-setup.sh
+
+
+
+
+
+
+
+
 
