e3512dffd6e1aa0dcd5da56031c921cd25c56b6b
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-06-09 21:00:22 -0400

    Update pgadmin4.tf

--- AWS-PGADMIN4/pgadmin4.tf
+++ AWS-PGADMIN4/pgadmin4.tf
@@ -61,26 +61,34 @@ resource "aws_instance" "pgadmin4-instance" {
       max_price = 0.004000
     }
   }
+  user_data =   user_data = <<-EOT
+#!/bin/bash
+sudo -i
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install -y postgresql12-server
+sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+sudo systemctl enable postgresql-12
+sudo systemctl start postgresql-12
+sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
+sudo yum install -y pgadmin4-web
+sudo yum install expect
+cat << 'EOF' > pgadmin-setup.sh
+#!/usr/bin/expect -f
+spawn sudo /usr/pgadmin4/bin/setup-web.sh --yes
+expect "Email address:"
+send "edwardli105@gmail.com\r"
+expect "Password:"
+send "password\r"
+expect "Retype Password:"
+send "password\r"
+interact
+EOF
+chmod +x pgadmin-setup.sh
+./pgadmin-setup.sh
+  EOT
    tags = {
     Name = "pgadmin4"
   }
 
-  provisioner "file" {
-    source      = "pgadmin4.sh"
-    destination = "/tmp/pgadmin4.sh"
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod u+x /tmp/pgadmin4.sh"
-      , "sudo /tmp/pgadmin4.sh"
-    ]
-
-  }
-
-  connection {
-    user        = "centos"
-    private_key = var.privid
-    host        = aws_instance.pgadmin4-instance.public_ip
-  }
 }
