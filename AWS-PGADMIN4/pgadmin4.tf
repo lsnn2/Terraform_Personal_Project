c3020df66a16c6bb8482dd62e7c7e19510f672f4
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-06-09 21:58:51 -0400

    Update pgadmin4.tf

--- AWS-PGADMIN4/pgadmin4.tf
+++ AWS-PGADMIN4/pgadmin4.tf
@@ -63,18 +63,17 @@ resource "aws_instance" "pgadmin4-instance" {
   }
   user_data = <<-EOT
 #!/bin/bash
-sudo -i
-sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-sudo yum install -y postgresql12-server
-sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
-sudo systemctl enable postgresql-12
-sudo systemctl start postgresql-12
-sudo rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
-sudo yum install -y pgadmin4-web
-sudo yum install -y expect
+yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+yum install -y postgresql12-server
+/usr/pgsql-12/bin/postgresql-12-setup initdb
+systemctl enable postgresql-12
+systemctl start postgresql-12
+rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
+yum install -y pgadmin4-web
+yum install -y expect
 cat << 'EOF' > pgadmin-setup.sh
 #!/usr/bin/expect -f
-spawn sudo /usr/pgadmin4/bin/setup-web.sh --yes
+spawn /usr/pgadmin4/bin/setup-web.sh --yes
 expect "Email address:"
 send "edwardli105@gmail.com\r"
 expect "Password:"
