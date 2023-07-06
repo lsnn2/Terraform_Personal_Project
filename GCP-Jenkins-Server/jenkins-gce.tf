9265237c81c899303473c6c32f5b764ea019be1b
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2023-07-06 16:28:26 -0400

    Update jenkins-gce.tf

--- GCP-Jenkins-Server/jenkins-gce.tf
+++ GCP-Jenkins-Server/jenkins-gce.tf
@@ -3,3 +3,19 @@ provider "google" {
   zone        = "northamerica-northeast2a"
 }
 
+resource "google_compute_instance" "jenkins-server" {
+  name         = "jenkins-server"
+  machine_type = "e2-micro"
+  
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
