8240c7506da447397896c898a47d53da447fd900
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2024-12-14 23:32:10 -0500

    update to ubuntu_24_04

--- AWS_LS_VPN/lightsail.tf
+++ AWS_LS_VPN/lightsail.tf
@@ -13,7 +13,7 @@ variable "pubid" {
 resource "aws_lightsail_instance" "outline-instance" {
   name              = "outline-instance"
   availability_zone = "eu-west-2a"
-  blueprint_id      = "ubuntu_18_04"
+  blueprint_id      = "ubuntu_24_04"
   bundle_id         = "nano_2_0"
   key_pair_name     = aws_lightsail_key_pair.outline-key.name
   provisioner "file" {
