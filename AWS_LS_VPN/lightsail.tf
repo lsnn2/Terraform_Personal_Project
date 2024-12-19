1aa2de949bbac89b53d4b342f8a4aced1d242b2b
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2024-12-19 04:41:50 -0500

    Update lightsail.tf

--- AWS_LS_VPN/lightsail.tf
+++ AWS_LS_VPN/lightsail.tf
@@ -12,9 +12,9 @@ variable "pubid" {
 
 resource "aws_lightsail_instance" "outline-instance" {
   name              = "outline-instance"
-  availability_zone = "ap-northeast-1a"
+  availability_zone = "ap-northeast-1c"
   blueprint_id      = "ubuntu_24_04"
-  bundle_id         = "nano_3_0"
+  bundle_id         = "micro_3_0"
   key_pair_name     = aws_lightsail_key_pair.outline-key.name
   provisioner "file" {
     source      = "DeployOutlineVPN.sh"
