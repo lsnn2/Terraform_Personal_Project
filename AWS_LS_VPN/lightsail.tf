f8020746d74b38164cd3e451bf9acd03f58dccfe
lsnn2 <121289430+lsnn2@users.noreply.github.com>
2025-12-05 12:10:46 +0800

    Update lightsail.tf

--- AWS_LS_VPN/lightsail.tf
+++ AWS_LS_VPN/lightsail.tf
@@ -12,7 +12,7 @@ variable "pubid" {
 
 resource "aws_lightsail_instance" "outline-instance" {
   name              = "outline-instance"
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "ap-northeast-2c"
   blueprint_id      = "ubuntu_24_04"
   bundle_id         = "micro_3_0"
   key_pair_name     = aws_lightsail_key_pair.outline-key.name
